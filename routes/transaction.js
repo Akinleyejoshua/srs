f0b2952d1907c12865474c53a769ab4f80156aff
Joshua <akinleyejoshua.dev@gmail.com>
2023-03-17 16:09:02 +0100

    Add files via upload

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const {getTransactions, saveTransaction} = require("../controllers/transaction");
 const authToken = require("../middlewares/auth");
 
-router.post("/", authToken, getTransactions);
+router.get("/", authToken, getTransactions);
 router.post("/update", authToken, saveTransaction);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const {getUserData, updateUserData} = require("../controllers/user");
 const authToken = require("../middlewares/auth");
 
-router.post("/", authToken, getUserData);
+router.get("/", authToken, getUserData);
 router.post("/update", authToken, updateUserData);
 
 module.exports = router;
